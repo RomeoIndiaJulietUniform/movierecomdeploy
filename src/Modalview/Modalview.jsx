db69c5a05b7909e0fa589c112c9e727e4c7f09de
Riju Mondal <pilotrijurocks@gmail.com>
2024-01-19 08:46:13 +0530

    updated css

--- src/Modalview/Modalview.jsx
+++ src/Modalview/Modalview.jsx
@@ -6,7 +6,7 @@ const PrivacyModal = (props) => {
     const [open, setOpen] = React.useState(false);
     const policyText = (
         <p>
-            Greetings! I'm a skilled web developer currently transitioning into Full Stack Development. Proficient in React, JavaScript, TypeScript, TailwindCSS, Electron, NodeJS, and Express JS, I graduated from Jadavpur University in 2022. My expertise extends to tools like Jira, PostgreSQL, Agile Methodology, Databricks, DBeaver, Snowflake, and SFDC. Beyond coding, I'm passionate about mastering Data Structures and Algorithms, with a focus on Dynamic Programming, Hash Tables, Trees, and Graphs. Eager to engage with the community, I bring a wealth of experience in software development and a commitment to continuous learning and growth. Let's connect and thrive together!
+            Veer Bhogya Basundhara 
         </p>
     );
     return (
